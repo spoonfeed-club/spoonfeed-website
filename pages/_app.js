1bcde7bc3a4b28371834c2630444dcea627967a2
Abhimanyu Jha <abhimanyu.in@gmail.com>
2023-02-11 18:17:06 +0530

    awesthetic update

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,19 @@
 // pages/_app.js
 import { ChakraProvider } from "@chakra-ui/react";
+import { extendTheme } from "@chakra-ui/react";
+
+// 2. Add your color mode config
+const config = {
+	initialColorMode: "dark",
+	useSystemColorMode: false,
+};
+
+// 3. extend the theme
+const theme = extendTheme({ config });
 
 function MyApp({ Component, pageProps }) {
 	return (
-		<ChakraProvider>
+		<ChakraProvider theme={theme}>
 			<Component {...pageProps} />
 		</ChakraProvider>
 	);
--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,29 @@
 import { Center, Heading, Text, VStack } from "@chakra-ui/react";
+import Link from "next/link";
 
 export default function Home() {
 	return (
-		<Center minH="100vh" minW="100vw">
-			<VStack alignItems="center" justifyContent="center" spacing="5">
-				<Heading>Tasty things are cooking 🥣!</Heading>
-				<Text color="gray.400">
-					Welcome to spoonfeed.club, we'll be live soon!
+		<Center minH="100vh" minW="100vw" background={"#222222"}>
+			<VStack
+				alignItems="center"
+				justifyContent="center"
+				spacing="5"
+				px="24"
+			>
+				<Heading color={"#FFFFE3"} fontWeight="black">
+					Tasty things are cooking 🥣!
+				</Heading>
+				<Text color="#FFFFE3" opacity={0.7}>
+					Welcome to spoonfeed.club, we'll be live soon! Follow us on{" "}
+					<Link
+						href="https://www.instagram.com/spoonfeed.club"
+						style={{
+							textDecoration: "underline",
+						}}
+					>
+						Instagram
+					</Link>{" "}
+					for updates.
 				</Text>
 			</VStack>
 		</Center>
