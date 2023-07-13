be9acf07952c946de08ae3cef88c08002df94cc6
Abhimanyu Jha <abhimanyu.in@gmail.com>
2023-07-13 19:28:03 +0530

    mixpanel test

--- pages/_app.js
+++ pages/_app.js
@@ -11,6 +11,27 @@ const config = {
 // 3. extend the theme
 const theme = extendTheme({ config });
 
+import mixpanel from "mixpanel-browser";
+
+// Replace YOUR_TOKEN with your Project Token
+// Note: This is configured to track an event for every page view automatically. We also recommend
+//       using localStorage instead of the default cookie for persistence.
+mixpanel.init("79c3ce279ee92fcf3a7b5ff53a7c7fdb", {
+	debug: true,
+	track_pageview: true,
+	persistence: "localStorage",
+});
+
+// Set this to a unique identifier for the user performing the event.
+// eg: their ID in your database or their email address.
+mixpanel.identify("abhimanyu.in@gmail.com");
+
+// Track an event. It can be anything, but in this example, we're tracking a Signed Up event.
+// Include a property about the signup, like the Signup Type
+mixpanel.track("Signed Up", {
+	"Signup Type": "Referral",
+});
+
 function MyApp({ Component, pageProps }) {
 	return (
 		<ChakraProvider theme={theme}>
